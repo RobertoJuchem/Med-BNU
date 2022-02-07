b68ad5220b70e43f9dd5b4c82a6d2d609e962eda
RobertoJuchem <juchemroberto@gmail.com>
2022-02-07 17:33:14 -0300

    starting javascript implementation

---
+++ javascript/legalPerson.js
@@ -0,0 +1,11 @@
+export class LegalPerson {
+    
+   constructor (firstAgeGroup, secondAgeGroup, thirdAgeGroup, fourthAgeGroup, fifthAgeGroup, sixthAgeGroup) {
+      this.firstAgeGroup = firstAgeGroup
+      this.secondAgeGroup = secondAgeGroup
+      this.thirdAgeGroup = thirdAgeGroup
+      this.fourthAgeGroup = fourthAgeGroup
+      this.fifthAgeGroup = fifthAgeGroup
+      this.sixthAgeGroup = sixthAgeGroup
+   }
+}
---
+++ javascript/priceTable.js
@@ -0,0 +1,10 @@
+export class PriceTable {
+   constructor(
+      firstPriceRange = 121.98,
+      secondPriceRange = 196.69,
+      thirdPriceRange = 267.32,
+      fourthPriceRange = 459.47,
+      fifthPriceRange = 674.51,
+      sixthPriceRange = 933.21
+      ){}
+}
